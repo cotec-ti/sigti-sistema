051dd77e5758cc2316c5865080d6829b9b96b40f
mouzerskt8-cmyk <mouzerskt8@gmail.com>
2026-03-17 17:49:57 -0300

    Salvando tudo para mover

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
 /* ============================================================ 
    CONEXÃO OFICIAL - SIGTI
    ============================================================ */
 const SB_URL = 'https://ilyhtxionytobqkerdka.supabase.co';
-const SB_KEY = 'sb_publishable_iNHedC8e8Ztp46umNmFEQQ_NV-vq8Kt'; 
+const SB_KEY = 'sb_publishable_iNHedC8e8Ztp46umNmFEQQ_NV-vq8Kt';
 
 // Inicializa o cliente do Supabase
-var supabaseClient = supabase.createClient(SB_URL, SB_KEY);
+//var supabaseClient = supabase.createClient(SB_URL, SB_KEY);
+
+var supabase = supabase.createClient(SB_URL, SB_KEY);
 
 // Variável para controle de sessão
 let currentUser = null;
@@ -15,22 +16,5 @@ let currentUser = null;
 supabaseClient.from('usuarios').select('count', { count: 'exact', head: true })
     .then(res => {
         console.log("Status da Conexão Supabase:", res.error ? "Erro: " + res.error.message : "Conectado com Sucesso!");
-=======
-/* ============================================================ 
-   CONEXÃO OFICIAL - SIGTI
-   ============================================================ */
-const SB_URL = 'https://ilyhtxionytobqkerdka.supabase.co';
-const SB_KEY = 'sb_publishable_iNHedC8e8Ztp46umNmFEQQ_NV-vq8Kt'; 
 
-// Inicializa o cliente do Supabase
-var supabaseClient = supabase.createClient(SB_URL, SB_KEY);
-
-// Variável para controle de sessão
-let currentUser = null;
-
-// Teste de conexão (aparecerá no F12 do navegador)
-supabaseClient.from('usuarios').select('count', { count: 'exact', head: true })
-    .then(res => {
-        console.log("Status da Conexão Supabase:", res.error ? "Erro: " + res.error.message : "Conectado com Sucesso!");
->>>>>>> a872a6567bc7b43f57391fe56831b21a218b638f
     });
