7402416c4a3b48f2a39db1a1d6023e4fc0515460
Mouzer <mouzerskt8@gmail.com>
2026-03-20 15:00:40 -0300

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-function fecharModal() {
-    document.getElementById('modal-termo').style.display = 'none';
-}
-
-function toggleTheme() {
-            document.body.classList.toggle('dark-mode');
-            localStorage.setItem('sigti-theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-        }         
-        if(localStorage.getItem('sigti-theme') === 'dark') document.body.classList.add('dark-mode');
